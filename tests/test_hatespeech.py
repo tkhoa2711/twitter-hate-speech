2e934389f3594a38dad33955b9f1f5dca36ab7c0
Khoa Le <ltkhoa2711@gmail.com>
2018-05-05 22:06:11 +1000

    add unit tests for hate speech module

---
+++ tests/test_hatespeech.py
@@ -0,0 +1,23 @@
+import unittest
+from api import hatespeech
+from api.database import mongo
+
+
+class HateSpeechTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            mongo.db.hateword.insert([
+                {'word': 'test'},
+                {'word': 'hate'},
+            ])
+        except Exception as e:
+            raise unittest.SkipTest(e)
+
+    @classmethod
+    def tearDownClass(cls):
+        mongo.db.hateword.remove({})
+
+    def test_get_hate_word_list(self):
+        assert hatespeech.get_hate_word_list() == ['test', 'hate']
