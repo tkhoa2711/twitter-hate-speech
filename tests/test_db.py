4a887cd8561934d301cbabc1acfe5341d3cbd4f5
Khoa Le <ltkhoa2711@gmail.com>
2018-05-06 21:38:02 +1000

    fix db test

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,11 +2,12 @@ import unittest
 import flask_pymongo
 from unittest import mock
 
+
 class DatabaseTestCase(unittest.TestCase):
 
     @mock.patch("flask_pymongo.PyMongo")
     def test_connection(self, mock_mongoclient):
         from api import database
-        db = database.db
+        db = database.mongo.db
         print(db)
         assert db is not None
