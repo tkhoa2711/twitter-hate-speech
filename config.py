70b4ff142c10ad4ac30e73f2ee7c5be11fae2ad8
Khoa Le <ltkhoa2711@gmail.com>
2018-05-06 21:27:16 +1000

    use PORT setting from environment if available

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class dotdict(dict):
 class Config:
     SECRET_KEY = 'testkey'
     MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/twitter')
+    PORT = os.environ.get('PORT', 5000)
 
 
 class DevelopmentConfig(Config):
