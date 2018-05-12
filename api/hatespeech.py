78d5f57d06c6f699181a709e78d1e7fab3cdedf6
Khoa Le <ltkhoa2711@gmail.com>
2018-05-13 08:44:01 +1000

    add some loggings

--- api/hatespeech.py
+++ api/hatespeech.py
@@ -1,5 +1,6 @@
 from api.database import db
 from api.app import app
+from api.logging import log
 from flask import Blueprint, jsonify, request
 
 
@@ -14,6 +15,7 @@ def _get_hate_word_list():
     """Retrieve the list of hate words."""
     import json
     from bson import json_util
+
     result = db.hateword.find()
     return jsonify(result=[
         json.loads(json.dumps(item, indent=4, default=json_util.default))
@@ -25,24 +27,31 @@ def _get_hate_word_list():
 def _set_hate_word():
     """Add a new hate word to the list or update existing one."""
     req = request.get_json()
-    db.hateword.replace_one({
-        'word': req['word']
-    },
-    {
+    obj = {
         'word': req['word'],
         'category': req.get('category'),
         'similar_to': req.get('similar_to'),
-    },
-    upsert=True)
+    }
+    result = db.hateword.replace_one({
+        'word': req['word']
+    }, obj, upsert=True)
+
+    if result.modified_count == 1:
+        log.info(f"Updated hate word [{req['word']}]: {obj}")
+    elif result.upserted_id is not None:
+        log.info(f"Added new hate word [{req['word']}]: {obj}")
 
 
 @app.route('/hatewords', methods=['DELETE'])
 def _delete_hate_word():
     """Delete a hate word from the list."""
     req = request.get_json()
-    db.hateword.delete_one({
+    result = db.hateword.delete_one({
         'word': req['word']
     })
+
+    if result.deleted_count == 1:
+        log.info(f"Deleted hate word [{req['word']}]")
 
 
 # ============================================================================
