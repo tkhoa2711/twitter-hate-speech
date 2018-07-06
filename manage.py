d65883310f9b2c115dcf6a55067f0bad9402cf52
Khoa Le <ltkhoa2711@gmail.com>
2018-07-06 22:15:45 +1000

    fix config for prod

--- manage.py
+++ manage.py
@@ -53,7 +53,7 @@ def prod():
     Run the server in production mode.
     """
     init_app()
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0', port=config.PORT, threaded=True)
 
 
 @manager.command
